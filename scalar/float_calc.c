61cad3d814c445d4395f42883cb28a2dec804e49
bin.wang <bin.wang@capstonegaming.com>
2020-09-07 11:59:31 +0800

    add scalar testing

---
+++ scalar/float_calc.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+int main(){
+	float u = 1.234e2;
+	printf("%.2f\n", u);
+	printf("%e\n", u);
+}
---
+++ scalar/int_calc.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int main(){
+	signed char u = -123;
+	signed char v = -5;
+	signed char r = u +v;
+	printf("%d", r );
+}
