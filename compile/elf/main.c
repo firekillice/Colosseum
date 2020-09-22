f923272af9d48234314126bd8eaa01c2ac7d61c3
bin.wang <bin.wang@capstonegaming.com>
2020-09-22 22:32:19 +0800

    add elf research

---
+++ compile/elf/main.c
@@ -0,0 +1,22 @@
+
+const char* str_a = "testing-a";
+const char* str_b = "testing-b";
+
+int g_i = 30;
+int g_j = 40;
+
+int g_zero = 0;
+
+int local_func(int x) {
+	x = 30;
+	int y = 40;
+}
+
+
+int printf(const char* format, ...);
+
+int main() {
+
+
+
+}
