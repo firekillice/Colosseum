9c6a107e7f46f971dbf7c8b1cdca50523eef1d1e
firekillice <firekillice@163.com>
2023-08-19 21:25:48 +0800

    update

--- os/tiny/build.sh
+++ os/tiny/build.sh
@@ -1,2 +1,3 @@
 nasm -f bin tiny.asm -o tiny
 od -t x1 -A x tiny
+chmod +x tiny
--- os/tiny/tiny.asm
+++ os/tiny/tiny.asm
@@ -1,21 +1,24 @@
+org 0x7c00
+
+jmp Entry
+
 %include "./disk_load.asm"
 %include "./util.asm"
 
-org 0x7c00
-mov bx, HELLO
-call print_string
-
-mov bx, BYE
-call print_string
-
-jmp $
+Entry:
+	call clean_screen
+	call set_cursor
+	mov si, HELLO
+	call print_string
+	mov si, BYE
+	call print_string
+	
+	jmp $
 
 HELLO:
-	db 'Hello,OS!'
+	db 'Hello,OS!',0x0a,0
 BYE:
-	db 'GoodBye!'
+	db 'GoodBye!',0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
-
-
