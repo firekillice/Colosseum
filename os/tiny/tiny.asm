70c8219661e8f115c4a9878eb9bfa47a0cced813
firekillice <firekillice@163.com>
2023-09-02 19:57:31 +0800

    update tiny

--- os/tiny/tiny.asm
+++ os/tiny/tiny.asm
@@ -10,15 +10,21 @@ Entry:
 	call set_cursor
 	mov si, HELLO
 	call print_string
+
+	call disk_load
+
 	mov si, BYE
 	call print_string
 	
 	jmp $
 
 HELLO:
-	db 'Hello,OS!',0x0a,0
+	db 'Hello,OS!',0x0a,0x0d,0
 BYE:
 	db 'GoodBye!',0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
